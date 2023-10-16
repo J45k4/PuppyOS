67babb137d5a34a5e483ae024b15bc9314aacd44
J45k4 <J45k4@users.noreply.github.com>
2023-10-16 03:42:48 +0000

    deploy: 202cea440d5ad2f45f4edd63f45d91be08a82667

--- js/app.js
+++ js/app.js
@@ -114,7 +114,7 @@ window.onload = () => {
 };
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
-        navigator.serviceWorker.register("/service-worker.js").then(function (registration) {
+        navigator.serviceWorker.register("/PuppyOS/service-worker.js").then(function (registration) {
             // Registration was successful
             console.log("ServiceWorker registration successful with scope: ", registration.scope);
         }, function (err) {

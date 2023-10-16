aa15b3d40f23e446b2408ed5c600429144a8dab7
J45k4 <J45k4@users.noreply.github.com>
2023-10-16 04:12:50 +0000

    deploy: 3956bdbf0695cdde6aa267802c92785b389edd20

--- js/app.js
+++ js/app.js
@@ -112,14 +112,16 @@ window.onload = () => {
     // })
     // desktop.root.appendChild(icon2.root)
 };
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function () {
-        navigator.serviceWorker.register("/PuppyOS/service-worker.js").then(function (registration) {
-            // Registration was successful
-            console.log("ServiceWorker registration successful with scope: ", registration.scope);
-        }, function (err) {
-            // registration failed :(
-            console.log("ServiceWorker registration failed: ", err);
+if (location.protocol === "https:") {
+    if ("serviceWorker" in navigator) {
+        window.addEventListener("load", function () {
+            navigator.serviceWorker.register("/PuppyOS/service-worker.js").then(function (registration) {
+                // Registration was successful
+                console.log("ServiceWorker registration successful with scope: ", registration.scope);
+            }, function (err) {
+                // registration failed :(
+                console.log("ServiceWorker registration failed: ", err);
+            });
         });
-    });
+    }
 }
